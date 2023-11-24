107791a4ccc997b630c763353e3ed0c1ca03798d
Kyle Stemen <bluelightning32@gmail.com>
2023-11-24 02:10:28 -0800

    Add skeletons for the scope block and block entity classes

---
+++ src/LambdaFactoryModSystem.cs
@@ -0,0 +1,20 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace LambdaFactory {
+  public class LambdaFactoryModSystem : ModSystem {
+    public override void Start(ICoreAPI api) {
+      api.RegisterBlockClass("Scope", typeof(BlockScope));
+      api.RegisterBlockEntityClass("Scope", typeof(BlockEntityScope));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api) {
+
+    }
+
+    public override void StartServerSide(ICoreServerAPI api) {
+
+    }
+  }
+}
