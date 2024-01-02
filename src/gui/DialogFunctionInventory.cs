84573051a47f882f7828bfceb6e8856392a98c77
Kyle Stemen <bluelightning32@gmail.com>
2024-01-02 00:34:08 -0800

    Add an error tab to the function dialog

--- src/gui/DialogFunctionInventory.cs
+++ src/gui/DialogFunctionInventory.cs
@@ -6,19 +6,62 @@ using Vintagestory.API.MathTools;
 namespace Lambda.Gui;
 
 public class DialogFunctionInventory : GuiDialogBlockEntity {
-  private static int _instance = 0;
+  private int _tab = 0;
+  private string _errorMessage = "error message not set";
+  private string _description;
+  public string Description {
+    get { return _description; }
+    set {
+      _description = value;
+      SingleComposer.GetRichtext("description")
+          .SetNewText(_description, CairoFont.WhiteSmallText());
+    }
+  }
+
   public DialogFunctionInventory(string title, string description,
                                  InventoryBase inventory, BlockPos blockPos,
                                  ICoreClientAPI capi)
-      : base(title, inventory, blockPos, capi) {
-    int instance = _instance++;
+      : base(Lang.Get(title), inventory, blockPos, capi) {
+    _description = description;
+    ClearComposers();
+    SingleComposer = capi.Gui.CreateCompo("terminventory" + BlockEntityPosition,
+                                          ElementBounds.Empty);
+    Compose();
+  }
+
+  public void Compose() {
     ElementBounds dialogBounds =
         ElementStdBounds.AutosizedMainDialog
             .WithAlignment(EnumDialogArea.RightMiddle)
             .WithFixedAlignmentOffset(-GuiStyle.DialogToScreenPadding, 0);
 
+    const int buttonHeight = 25;
+    ElementBounds tabBounds =
+        ElementBounds.Fixed(0, -buttonHeight, 300, buttonHeight);
+
+    GuiTab[] tabs = new GuiTab[] {
+      new() { Name = Lang.Get("lambda:tab-container"), DataInt = 0 },
+      new() { Name = Lang.Get("lambda:tab-errors"), DataInt = 1 },
+    };
+
     ElementBounds bgBounds = ElementBounds.Fill;
     bgBounds.BothSizing = ElementSizing.FitToChildren;
+
+    SingleComposer.Clear(dialogBounds);
+    SingleComposer =
+        capi.Gui
+            .CreateCompo("terminventory" + BlockEntityPosition, dialogBounds)
+            .AddHorizontalTabs(tabs, tabBounds, OnTabClicked,
+                               CairoFont.ButtonText().WithFontSize(
+                                   (float)GuiStyle.SmallFontSize),
+                               CairoFont.ButtonPressedText().WithFontSize(
+                                   (float)GuiStyle.SmallFontSize),
+                               "tabs")
+            .AddShadedDialogBG(bgBounds)
+            .AddDialogTitleBar(DialogTitle, "title")
+            .BeginChildElements(bgBounds);
+
+    SingleComposer.GetHorizontalTabs("tabs").SetValue(_tab, false);
 
     ElementBounds textBounds =
         ElementBounds
@@ -32,41 +75,53 @@ public class DialogFunctionInventory : GuiDialogBlockEntity {
                       0, 1, 1)
             .FixedUnder(textBounds, -GuiStyle.ElementToDialogPadding)
             .WithFixedPadding(0, GuiStyle.ElementToDialogPadding);
+    if (_tab == 0) {
+      ElementBounds buttonBounds =
+          ElementStdBounds.MenuButton(0, EnumDialogArea.LeftTop)
+              .WithFixedSize(150, buttonHeight)
+              .FixedUnder(textBounds, 2)
+              .FixedRightOf(gridBounds, GuiStyle.ElementToDialogPadding);
 
-    ElementBounds buttonBounds =
-        ElementStdBounds.MenuButton(0, EnumDialogArea.LeftTop)
-            .WithFixedSize(150, 25)
-            .FixedUnder(textBounds)
-            .FixedRightOf(gridBounds, GuiStyle.ElementToDialogPadding);
+      ElementBounds progressBounds =
+          ElementStdBounds
+              .Statbar(EnumDialogArea.LeftTop,
+                       buttonBounds.fixedWidth + 2 * buttonBounds.fixedPaddingX)
+              .FixedUnder(buttonBounds,
+                          GuiStyle.HalfPadding + buttonBounds.fixedPaddingY)
+              .FixedRightOf(gridBounds, GuiStyle.ElementToDialogPadding)
+              .WithFixedPadding(GuiStyle.ElementToDialogPadding / 2);
 
-    ElementBounds progressBounds =
-        ElementStdBounds
-            .Statbar(EnumDialogArea.LeftTop,
-                     buttonBounds.fixedWidth + 2 * buttonBounds.fixedPaddingX)
-            .FixedUnder(buttonBounds,
-                        GuiStyle.HalfPadding + buttonBounds.fixedPaddingY)
-            .FixedRightOf(gridBounds, GuiStyle.ElementToDialogPadding)
-            .WithFixedPadding(GuiStyle.ElementToDialogPadding / 2);
+      SingleComposer
+          .AddRichtext(Lang.Get(Description), CairoFont.WhiteSmallText(),
+                       textBounds, "description")
+          .AddItemSlotGrid(Inventory, DoSendPacket, 1, gridBounds)
+          .AddSmallButton(Lang.Get("lambda:inscribe"), OnInscribe, buttonBounds)
+          .AddStatbar(progressBounds, GuiStyle.SuccessTextColor, true,
+                      "progress");
+    } else {
+      textBounds =
+          ElementBounds
+              .Fixed(GuiStyle.ElementToDialogPadding,
+                     GuiStyle.TitleBarHeight + GuiStyle.ElementToDialogPadding,
+                     300, 200)
+              .WithFixedPadding(GuiStyle.ElementToDialogPadding / 2, 0);
+      SingleComposer.AddRichtext(_errorMessage, CairoFont.WhiteSmallText(),
+                                 textBounds, "errors");
+      // The gridBounds represents the last vertical element on the other tab.
+      // Even though the error tab does not include the grid, explicitly add its
+      // bound here so that error tab is the same height as the container tab.
+      bgBounds.WithChild(gridBounds);
+    }
 
-    ClearComposers();
-    description = Lang.Get(description);
-    SingleComposer =
-        capi.Gui
-            .CreateCompo("terminventory" + BlockEntityPosition, dialogBounds)
-            .AddShadedDialogBG(bgBounds)
-            .AddDialogTitleBar(Lang.Get(title), "title")
-            .BeginChildElements(bgBounds)
-            .AddRichtext(description, CairoFont.WhiteSmallText(), textBounds,
-                         "description")
-            .AddItemSlotGrid(Inventory, DoSendPacket, 1, gridBounds)
-            .AddSmallButton(Lang.Get("lambda-inscribe"), OnInscribe,
-                            buttonBounds)
-            .AddStatbar(progressBounds, GuiStyle.SuccessTextColor, true,
-                        "progress")
-            .EndChildElements();
+    SingleComposer.EndChildElements();
     bgBounds.CalcWorldBounds();
     SingleComposer.Compose();
   }
 
   private bool OnInscribe() { return true; }
+
+  private void OnTabClicked(int tab) {
+    _tab = tab;
+    Compose();
+  }
 }
